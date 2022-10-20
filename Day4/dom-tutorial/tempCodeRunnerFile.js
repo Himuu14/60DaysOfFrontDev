78ce47d3326c2cb99726122cc217e108420a1058
Himansh Mishra <88498643+Himuu14@users.noreply.github.com>
2022-10-20 18:38:04 +0530

    day 4,6,7

---
+++ Day4/dom-tutorial/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+
+// // for(let navItem of navItems){
+// //     navItem.style.backgroundColor = "#fff";
+// //     navItem.style.color = "green";
+// //     navItem.style.fontWeight = "bold";
+// // }
+// // navItems.forEach((navItem)=>{
+// //     navItem.style.backgroundColor = "#fff";
+// //     navItem.style.color = "green";
+// //     navItem.style.fontWeight = "bold";
+// // })
+// // console.log(navItems);
---
+++ Day6/REACT - Learning/myfirstapp/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+//import App from './App';
+
+import reportWebVitals from './reportWebVitals';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <h1>Hello World </h1>
+);
+
+reportWebVitals();
